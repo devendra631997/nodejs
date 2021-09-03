a136c394a8f91179fd1445fd23d3c71f5575532e
devendra <you@example.com>
2021-09-04 00:53:15 +0530

    addded sentiment analysis api

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 var express = require('express')
 var app = express()
+app.use(express.json());
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
+})
+
+app.post('/sentiment', async function(req, res) {
+if(req.body){
+  if(req.body.TextArr){
+    var mydocx =req.body.TextArr
+    var arr = []
+  await mydocx.forEach(function(s){
+    arr.push(sentiment.analyze(s));
+  })
+  res.status(200).send(arr)
+  }
+  else{
+    res.status(500).send(`Body should be Like this => {
+      "TextArr": ["I hate apples","I don't eat pepper","the movie was very nice","this book is the best"]
+  } `)
+  }
+
+}else{
+  res.status(500).send(`Body should be Like this => {
+    "TextArr": ["I hate apples","I don't eat pepper","the movie was very nice","this book is the best"]
+} `)
+}
 })
 
 app.listen(app.get('port'), function() {
